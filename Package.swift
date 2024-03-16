6a57f96ec1410bc76998218e2b542f8f619ac8c0
Sergey Starukhin <star.s@me.com>
2024-03-16 12:35:58 +0100

    add dependencies

--- Package.swift
+++ Package.swift
@@ -17,11 +17,15 @@ let package = Package(
             name: "JsonRpcService",
             targets: ["JsonRpcService"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/star-s/HttpClient.git", from: "0.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JsonRpcService"),
+            name: "JsonRpcService",
+            dependencies: ["HttpClient"]),
         .testTarget(
             name: "JsonRpcServiceTests",
             dependencies: ["JsonRpcService"]),
