282d2b354c451bd0a56660a555090d7441972a3b
Sergey Starukhin <star.s@me.com>
2024-03-20 13:14:16 +0100

    refactoring rpc call tests

--- Tests/JsonRpcClientTests/RpcCallTests.swift
+++ Tests/JsonRpcClientTests/RpcCallTests.swift
@@ -9,42 +9,92 @@ import XCTest
 @testable import JsonRpcClient
 import JsonRpcClientMocks
 
-final class JsonRpcServiceTests: XCTestCase {
+final class RpcCallTests: XCTestCase {
 
-    let client = OpenRpcClient(
+    actor NotificationHandler {
+        var updateNotificationDidHandled = false
+        var foobarNotificationDidHandled = false
+
+        func resetNotifications() {
+            updateNotificationDidHandled = false
+            foobarNotificationDidHandled = false
+        }
+
+        func updateNotification(request: JsonRpc.Request) async {
+            guard updateNotificationDidHandled == false else {
+                XCTFail("Already handled!")
+                return
+            }
+            updateNotificationDidHandled = true
+        }
+
+        func foobarNotification(request: JsonRpc.Request) async {
+            guard foobarNotificationDidHandled == false else {
+                XCTFail("Already handled!")
+                return
+            }
+            foobarNotificationDidHandled = true
+        }
+    }
+
+    private lazy var client = OpenRpcClient(
         endpoint: "/",
         transport: JsonRpcServiceMock(
             implementations: [
                 "subtract": JsonRpcServiceMock.subtract(request:response:),
                 "sum": JsonRpcServiceMock.sum(request:response:),
                 "get_data": JsonRpcServiceMock.getData(request:response:),
+            ], notifications: [
+                "update": notificationHandler.updateNotification(request:),
+                "foobar": notificationHandler.foobarNotification(request:),
             ]
         ).withLogger()
     )
 
-    func testJsonRpcService() async throws {
+    private let notificationHandler = NotificationHandler()
+
+    func testRpcCallWithPositionalParameters() async throws {
         let result1: Int = try await client.invoke(method: "subtract", params: [42, 23])
         XCTAssertEqual(result1, 19)
         let result2: Int = try await client.invoke(method: "subtract", params: [23, 42])
         XCTAssertEqual(result2, -19)
-        let result3: Int = try await client.invoke(method: "subtract", params: [
+    }
+
+    func testRpcCallWithNamedParameters() async throws {
+        let result1: Int = try await client.invoke(method: "subtract", params: [
             "subtrahend" : 23,
             "minuend": 42,
         ])
-        XCTAssertEqual(result3, 19)
-        let result4: Int = try await client.invoke(method: "subtract", params: [
+        XCTAssertEqual(result1, 19)
+        let result2: Int = try await client.invoke(method: "subtract", params: [
             "minuend": 42,
             "subtrahend" : 23,
         ])
-        XCTAssertEqual(result4, 19)
+        XCTAssertEqual(result2, 19)
+    }
+
+    func testNotification() async throws {
+        await notificationHandler.resetNotifications()
+
         try await client.notify(method: "update", params: [1, 2, 3, 4])
+        let updateNotificationDidHandled = await notificationHandler.updateNotificationDidHandled
+        XCTAssertTrue(updateNotificationDidHandled)
+
         try await client.notify(method: "foobar", params: Parameters.void)
+        let foobarNotificationDidHandled = await notificationHandler.foobarNotificationDidHandled
+        XCTAssertTrue(foobarNotificationDidHandled)
+    }
+
+    func testRpcCallOfNonExistentMethod() async throws {
         do {
             let _: String = try await client.invoke(method: "foobar", params: Parameters.void)
         } catch let error as JsonRpcError {
             XCTAssertEqual(error.code, -32601)
             XCTAssertEqual(error.message, "Method not found")
         }
+    }
+
+    func testRpcCallEithAnEmptyArray() async throws {
         do {
             let _ = try await client.performBatch(requests: [])
         } catch let error as JsonRpcError {
@@ -82,14 +132,6 @@ extension JsonRpcServiceMock {
     }
 
     static func getData(request: JsonRpc.Request, response: JsonRpc.Response.Builder) async {
-        //
-    }
-
-    static func updateNotification(request: JsonRpc.Request) async {
-        //
-    }
-
-    static func foobarNotification(request: JsonRpc.Request) async {
-        //
+        XCTFail("Not yet implemented!")
     }
 }
